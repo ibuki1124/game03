35108a816a487cbb23a0272f0aa47fc4f0d5a87d
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-06-06 14:04:06 +0900

    [Add] 盤面の描画

--- 03.js
+++ 03.js
@@ -1,3 +1,24 @@
 // canvasのエレメント取得
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+let block_size = 30; // 盤面のサイズ
+let col = 20; //盤面の楯列
+canvas.height = col * block_size; //盤面の高さ
+let row = 10; //盤面の横列
+canvas.width = row * block_size; //盤面の横幅
+let board = [];
+
+// 盤面の1ブロックずつ配列化
+function drawBoard(col, row){
+    for (let i = 0; i < col; i++){
+        board.push([]);
+        for (let j = 0; j < row; j++){
+            board[i][j] = 0;
+            // 1ブロックずつ枠線の表示
+            ctx.strokeRect(j * block_size, i * block_size, block_size, block_size);
+        }
+    }
+}
+
+drawBoard(col, row);
